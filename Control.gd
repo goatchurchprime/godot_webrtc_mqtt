f8c9eeb3fd542c195a85c81a55e5ccafb16a60a7
Julian Todd <julian@goatchurch.org.uk>
2021-07-26 18:02:03 +0100

    made the webrtc work

--- Control.gd
+++ Control.gd
@@ -1,123 +1,77 @@
 extends Control
 
 
-var rtc_mp: WebRTCMultiplayer = WebRTCMultiplayer.new()
-var sealed = false
+var peer = WebRTCPeerConnection.new()
+var datachannel = null
 
-
-func Lset_remote_description(type: String, sdp: String):
-	print(" Lset_remote_description ")
-	print("TYPE: ", type)
-	print("SDP: ", sdp)
-	print("END")
-
-#var p1 = WebRTCPeerConnection.new()
-#var ch1 = p1.create_data_channel("chat", {"id": 1, "negotiated": true})
+var metopic = ""
+var othertopic = ""
 
 func _process(delta):
-	#p1.poll()
-	rtc_mp.poll()
-	
-func _ready():
-	print(rtc_mp, rtc_mp.get_unique_id())
-	#p1.connect("session_description_created", self, "Lset_remote_description")
+	peer.poll()
+	if datachannel != null and datachannel.get_ready_state() == datachannel.STATE_OPEN and datachannel.get_available_packet_count() > 0:
+		print("datachannel received: ", datachannel.get_packet().get_string_from_utf8())
 
-func stop():
-	rtc_mp.close()
+func _on_SetMeTopic_toggled(button_pressed):
+	if button_pressed:
+		
+		print($metopic.items, $metopic.selected)
+		metopic = $metopic.get_item_text($metopic.selected)
+		othertopic = $metopic.get_item_text(1-$metopic.selected)
+		print("Metopic: ", metopic, " othertopic: ", othertopic)		
 
-var Ldatachannel = null
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		#if event.scancode == KEY_M:
-		#	var x = p1.create_offer()
-		#	print("create offer err=", x)
-		if event.scancode == KEY_L:
-			var id = 1313131313			
-			var peer: WebRTCPeerConnection = WebRTCPeerConnection.new()
-			peer.initialize({"iceServers": [ { "urls": ["stun:stun.l.google.com:19302"] } ] })
-			peer.connect("session_description_created", self, "_offer_created", [id])
-			peer.connect("ice_candidate_created", self, "_new_ice_candidate", [id])
+		var uniqstring = OS.get_unique_id().replace("{", "").split("-")[0].to_upper().substr(0,4)
+		$mqttnode.server = "mosquitto.doesliverpool.xyz"
+		randomize()
+		$mqttnode.client_id = "u%s%da%d"%[uniqstring, int(metopic), randi()]
+		print("MQTT client_id:", $mqttnode.client_id)
+		$mqttnode.set_last_will(metopic+"status", "stopped", true)
+		if yield($mqttnode.connect_to_server(), "completed"):
+			$mqttnode.publish(metopic+"status", "connected", true)
+		else:
+			print("mqtt failed to connect")
 
-			rtc_mp.add_peer(peer, id)
-			#Ldatachannel = peer.create_data_channel("channel1", { "negotiated": true, "id": 1,  "maxRetransmits": 1, "ordered": true })
-			#print("Ldatachannel ", Ldatachannel)
+		$mqttnode.connect("received_message", self, "received_mqtt")
+		$mqttnode.subscribe(metopic+"offer")
+		$mqttnode.subscribe(metopic+"answer")
+		$mqttnode.subscribe(metopic+"ice")
+
+		peer.initialize({"iceServers": [ { "urls": ["stun:stun.l.google.com:19302"] } ] })
+		datachannel = peer.create_data_channel("chat", {"id": 1, "negotiated": true})
+		peer.connect("session_description_created", self, "_session_description_created")
+		peer.connect("ice_candidate_created", self, "_ice_candidate_created")
+
+		if $metopic.selected == 0:
 			var x = peer.create_offer()
 			print("peer create offer ", peer, "Error:", x)
-			
-		if event.scancode == KEY_M:
-			$MQTTExperiment.mqttpublish("yo", "datathing")
-						
-#There are two ways to create a working data channel: either call create_data_channel() 
-#on only one of the peer and listen to 
-#data_channel_received on the other, 
-#or call create_data_channel() on both peers, 
-#with the same values, and the negotiated option set to true.
 
-func _new_ice_candidate(mid_name, index_name, sdp_name, id):
-	print("nnnnew ice ", [mid_name, index_name, sdp_name, id])
-	#send_candidate(id, mid_name, index_name, sdp_name)
+	else:
+		print("Can only do once")
+		assert(false)
+		
+func _session_description_created(type, data):
+	print("_session_description_created ", [type, data.substr(0, 10)])
+	peer.set_local_description(type, data)
+	$mqttnode.publish(othertopic+type, data)
+	
+func _ice_candidate_created(mid_name, index_name, sdp_name):
+	print("_ice_candidate_created ", [mid_name, index_name, sdp_name])
+	$mqttnode.publish(othertopic+"ice", to_json([mid_name, index_name, sdp_name]))
 
+func received_mqtt(topic, message):
+	var stopic = topic.split("/")
+	print("MQTT RECEIVED: ", stopic, ": ", message.substr(0, 10))
+	if len(stopic) == 3:
+		assert (metopic == stopic[0]+"/"+stopic[1]+"/")
+		if stopic[2] == "offer":
+			peer.set_remote_description("offer", message)
+		elif stopic[2] == "answer":
+			peer.set_remote_description("answer", message)
+		elif stopic[2] == "ice":
+			var js = parse_json(message)
+			var e = peer.add_ice_candidate(js[0], js[1], js[2])
+			print("ICE error:", e)
 
-func _offer_created(type, data, id):
-	print("oooo ", [type, data, id])
-	$MQTTExperiment.mqttpublish(type, data)
-
-	if not rtc_mp.has_peer(id):
-		return
-	print("created", type)
-	rtc_mp.get_peer(id).connection.set_local_description(type, data)
-	if type == "offer": 
-		#return _send_msg("O", id, offer)
-		#send_offer(id, data)
-		pass
-	else: 
-		#_send_msg("A", id, answer)
-		#send_answer(id, data)
-		pass
-
-func connected(id):
-	print("Connected %d" % id)
-	rtc_mp.initialize(id, true)
-
-
-func lobby_joined(lobby):
-	self.lobby = lobby
-
-
-func lobby_sealed():
-	sealed = true
-
-
-func disconnected():
-	stop() # Unexpected disconnect
-
-
-func peer_connected(id):
-	print("Peer connected %d" % id)
-	#_create_peer(id)
-
-
-func peer_disconnected(id):
-	if rtc_mp.has_peer(id): rtc_mp.remove_peer(id)
-
-
-func offer_received(id, offer):
-	print("Got offer: %d" % id)
-	if rtc_mp.has_peer(id):
-		rtc_mp.get_peer(id).connection.set_remote_description("offer", offer)
-
-
-func answer_received(id, answer):
-	print("Got answer: %d" % id)
-	if rtc_mp.has_peer(id):
-		rtc_mp.get_peer(id).connection.set_remote_description("answer", answer)
-
-
-func candidate_received(id, mid, index, sdp):
-	if rtc_mp.has_peer(id):
-		rtc_mp.get_peer(id).connection.add_ice_candidate(mid, index, sdp)
-
-
-
-
-
+func _on_PingButton_pressed():
+	var e = datachannel.put_packet(("Ping from "+metopic+" yay!").to_utf8())
+	print("Ping Error: ", e)
---
+++ Immediate_code.gd
@@ -0,0 +1,12 @@
+tool
+extends EditorScript
+
+# *******
+# Control-Shift X to run this code in the editor
+# *******
+
+
+func _run():
+	var s = "sdfs/ddd/dddd3"
+	print(s.split("/"))
+	print(randi())
